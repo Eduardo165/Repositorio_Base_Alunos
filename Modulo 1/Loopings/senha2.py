f1b6bae1aef7b07989741d74cd8351df35641d04
Eduardo <eduardo.oliveira5024@gmail.com>
2025-09-11 13:49:13 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/senha2.py
@@ -0,0 +1,7 @@
+senha = input('Digite sua senha:')
+
+
+while senha != 'eduardo244':
+    print('Senha errada, digite novamente.')
+    senha = input('Digite sua senha: ')
+print('Senha CORRETA! ')
---
+++ Modulo 1/Loopings/tabuada.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um numero: '))  
+cont = 1
+while cont <= 10:
+    print(f'{cont} x {numero}: {numero*cont}')
+    cont = cont +1
